21b6f25c75a1476e972c2f4c71c6f39583c18464
veronica <vero911@naver.com>
2019-07-07 13:59:36 +0900

    Redirect to /Main from /

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,5 @@
 import React, {Component} from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
-import First from './pages/First/First';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
@@ -20,7 +15,7 @@ class Routes extends Component {
         <Router>
           <div>
             <Switch>
-              <Route exact path="/" component={First}/>
+              <Route exact path="/" render={() => (<Redirect to="/Main"/>)}/>
               <Route exact path="/Login" component={Login}/>
               <Route exact path="/Main" component={Main}/>
               <Route exact path="/Detail/:id" component={Detail}/>
