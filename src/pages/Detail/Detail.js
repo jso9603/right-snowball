ee00340994429bddf4a5c123b136d44b2d592b8d
JiSoo Park <jso9603@gmail.com>
2019-10-13 13:56:28 +0900

    Adding detail page

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,166 +1,110 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import BookmarkIcon from '@material-ui/icons/BookmarkBorder';
-import AlterIcon from '@material-ui/icons/CompareArrows';
+import Slider from "react-slick";
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './Detail.css';
 
-// JSON
-import { mock } from '../Data/mockData';
-const result = mock;
+const settings = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: true,
+  infinite: true,
+  arrows: false
+};
 
 class Detail extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      showPopup: false
+      showPopup: false,
+      selectedItem: 1,
+      cardData: [{
+        recommend: true,
+        title: '1',
+        description: 'No.1',
+        like: 13
+      },{
+        recommend: false,
+        title: '2',
+        description: 'No.2',
+        like: 7
+      },{
+        recommend: false,
+        title: '3',
+        description: 'No.3',
+        like: 2
+      }],
+      tags: ['갑질', '성차별', '강매', '식품', '대리점'],
+      description: '2019년 OO기업은 대리점 상대 상품 강매 논란으로 이슈가 되었다.'
     }
   }
 
-  componentWillMount() {
-    let { match: { params: { id } } } = this.props;
+  handleClickDot(idx) {
+    if (idx === this.state.selectedItem) return;
 
-    console.log('id: ', id);
+    this.setState({ selectedItem: idx });
 
-    for (let i = 0; i<result.topic.length; i++) {
-      if (result.topic[i].id === Number(id)) {
-        console.log('detailContent: ', result.topic[i]);
-      }
-    }
+    this.slider.slickGoTo(idx-1);
   }
-
 
   render() {
-    const { showPopup } = this.state;
+    const {
+      showPopup,
+      selectedItem,
+      cardData,
+      tags,
+      description
+    } = this.state;
+
     return (
-      <div className="detail-page" style={{ backgroundColor: showPopup && 'rgba(51, 51, 51, 0.85)'}}>
-        <button className="bookmark">
-          <BookmarkIcon style={{width: '39px', height: '39px'}}/>
-        </button>
-        <p className="title">인명피해<br/>가습기 살균제</p>
-        <p className="name">기업명 | 옥씨</p>
-        <p className="issue">이슈 | 영유아와 임산부 140여 명이 목숨을 잃은 가습기 살균제 사망사건</p>
-        <p className="news">
-          <span>참고기사 | </span>
-          <span style={{textDecoration: 'underline'}}>TF현장] 불매운동 그 후, 대형마트에서 후…</span>
-          <span style={{color: '#9e9e9e'}}>2018.03.26</span>
-        </p>
-        
-        <div className="keywords">
-          <hr/>
-          <div style={{marginBottom: '10px', textAlign: 'center' }}>
-            <span className="one" style={{ border: 'solid 1px #23a7ee', color: '#23a7ee' }}>#폐에_안좋은_제품(8)</span>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#살균제주의(20)</span>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#옥시_생활용품(150)</span>
-          </div>
-          <div style={{marginBottom: '10px', textAlign: 'center' }}>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#세탁용품(21)</span>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#방향제(4)</span>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#제습제(7)</span>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#청소용품(15)</span>
-          </div>
-          <div style={{marginBottom: '10px', textAlign: 'center' }}>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#세정제(3)</span>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#식기세척기세제(11)</span>
-            <span className="one" style={{ border: 'solid 1px #979797', color: '#333333' }}>#제모용품(3)</span>
-          </div>
+      <div className="detail-page">
+        <div className="slider-wrap">
+          <Slider 
+            ref={slider => (this.slider = slider)}
+            initialSlide={selectedItem}
+            afterChange={current=>this.handleClickDot(current)}
+            {...settings}
+          >
+            {cardData.map((el, idx) => {
+              return (
+                <div key={idx} className="portfolio-card">
+                  {el.title}
+                  <span className="like">
+                    <span className="like-count">{el.like}</span>
+                    <FavoriteBorder style={{color: 'red'}} />
+                  </span>
+                </div>
+            )})}
+          </Slider>
+
+          <ul className="slick-dots">
+            {cardData.map((el, idx) => (
+              <li key={idx} className={`${selectedItem === idx ? 'slick-active' : ''} `}>
+                <button />
+              </li>
+            ))}
+          </ul>
         </div>
 
-          <div className="thumbnail-container">
-            <div style={{display: 'inline-block'}}>
-              <div className="image-container">
-                <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-              </div>
-
-              <div className="thumbnail-title">
-                <span className="product-title">옥시싹싹</span>
-                <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-              </div> 
-            </div>
-            
-            <div style={{display: 'inline-block'}}>
-              <div className="image-container">
-                <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-              </div>
-              <div className="thumbnail-title">
-                <span className="product-title">아토오가닉</span>
-                <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-              </div>
-            </div>
-            
-            <div style={{display: 'inline-block'}}>
-              <div className="image-container">
-                <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-              </div>
-              <div className="thumbnail-title">
-                <span className="product-title">베지터블 홈 가습기클린업</span>
-                <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-              </div>
-            </div>
+        <div className="company-info">
+          <div style={{width: '100%', marginTop: '25px'}}>
+            <h5 className="company-name">OO기업</h5>
+            <div className="share"><ShareOutlinedIcon /></div>
           </div>
-
-          <div className="thumbnail-container">
-            <div style={{display: 'inline-block'}}>
-              <div className="image-container">
-                <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-              </div>
-
-              <div className="thumbnail-title">
-                <span className="product-title">옥시싹싹</span>
-                <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-              </div> 
-            </div>
-            
-            <div style={{display: 'inline-block'}}>
-              <div className="image-container">
-                <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-              </div>
-              <div className="thumbnail-title">
-                <span className="product-title">아토오가닉</span>
-                <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-              </div>
-            </div>
-            
-            <div style={{display: 'inline-block'}}>
-              <div className="image-container">
-                <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-              </div>
-              <div className="thumbnail-title">
-                <span className="product-title">베지터블 홈 가습기클린업</span>
-                <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-              </div>
-            </div>
+          <div>
+            {tags.map((tag, idx) => (
+              <span key={idx} className="company-tag">#{tag}&nbsp;</span>
+            ))}
           </div>
+          <p className="company-description">{description}</p>
 
-        
-
-        {/* <div className="title-container">
-          <div className="thumbnail-container">
-            <div className="image-container">
-              <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-            </div>
-            <div className="thumbnail-title">
-              <span className="product-title">옥시싹싹</span>
-              <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-            </div>
-
-            <div className="image-container">
-              <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-            </div>
-            <div className="thumbnail-title">
-              <span className="product-title">아토오가닉</span>
-              <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-            </div>
-            
-            <div className="image-container">
-              <img width="100" src="https://t1.daumcdn.net/cfile/tistory/230C2A435874E30F1E" alt="image1"/>
-            </div>
-            <div className="thumbnail-title">
-              <span className="product-title">베지터블 홈 가습기클린업</span>
-              <button style={{backgroundColor: 'transparent'}}><AlterIcon/></button>
-            </div>
-          </div>
-        </div> */}
+          <button className="btn-product-list">제품 리스트로 이동하기</button>
+          <button className="btn-news">관련 기사 페이지로 이동</button>
+        </div>
       </div>
     )
   }
