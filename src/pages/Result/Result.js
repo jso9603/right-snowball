ca412c562ba62c5694dac3abe09fa1e522eb224a
JiSoo Park <jso9603@gmail.com>
2019-10-27 12:42:53 +0900

    결과 페이지 업데이트

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -1,40 +1,29 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import CardSlider from '../../components/CardSlider/CardSlider';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import './Result.css';
+
+let likeList = [false, false];
 
 class Result extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      cardData: [{
-        recommend: true,
-        title: '1',
-        description: 'No.1',
-        like: 13
-      },{
-        recommend: false,
-        title: '2',
-        description: 'No.2',
-        like: 7
-      },{
-        recommend: false,
-        title: '3',
-        description: 'No.3',
-        like: 2
-      }],
       search: '식품',
       result: [
         {
           companyName: 'oo기업',
           tags: ['갑질', '성차별', '강매', '식품', '대리점'],
-          img: 'http://placehold.it/320x200'
+          img: 'http://placehold.it/320x200',
+          like: 1665
         },
         {
           companyName: 'xx기업',
           tags: ['뭘까', '뭐지', '뭐야'],
-          img: 'http://placehold.it/320x200'
+          img: 'http://placehold.it/320x200',
+          like: 10
         }
       ]
     }
@@ -55,26 +44,42 @@ class Result extends Component {
     return arrHTML;
   }
 
+  fullLike = (i) => {
+    const { result } = this.state;
+
+    likeList[i] = !likeList[i];
+    this.forceUpdate();
+  }
+
   render() {
     const {
-      cardData,
       search,
       result
     } = this.state;
 
+    const resultCtn = result.length;
+
     return (
       <div className="result-page">
-        <CardSlider data={cardData} />
         <h6 className="result-search">'{search}'와 관련된 기업 정보에요</h6>
 
         <div className="result-data-area">
-          {result.map((data, idx) => (
-            <div key={idx}>
+          {this.state.result.map((data, idx) => (
+            <div key={idx} className="result-idx">
+              <img src={data.img} className="result-company-image" alt="company" />
+
+              <span className="result-like" onClick={_ => this.fullLike(idx)}>
+                <span className="result-like-count">{data.like}</span>
+                {likeList[idx] ? (
+                  <FavoriteIcon style={{color: 'red'}} />
+                ) : (
+                  <FavoriteBorder style={{color: 'red'}} />
+                )}
+              </span>
+
               <h5 className="result-company-name">{data.companyName}</h5>
               {this.showTags(idx)}
-
-              <div className="result-horizontal-line"></div>
-              <img src={data.img} className="result-company-image" alt="company"></img>
+              <div className={`result-horizontal-line ${idx === (resultCtn-1) ?' noneBorder' : ''}`}></div>
             </div>
           ))}
         </div>
