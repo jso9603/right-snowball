64051101e7c5d78eb21a924f9c0cb3d7f3ac59f7
JiSoo Park <jso9603@gmail.com>
2020-01-19 16:49:09 +0900

    detail 페이지 header 부분 수정

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
-import CardSlider from '../../components/CardSlider/CardSlider';
+import CheckIcon from '@material-ui/icons/Check';
 import enterprises from '../Data/enterprise.json';
 import BarChart from '../../components/Chart/BarChart';
 import './Detail.css';
@@ -16,17 +16,35 @@ class Detail extends Component {
     this.setState({...entObject})
   }
 
-  renderTags = () => {
-    const { tags } = this.state;
-    let arrHTML = [];
+  renderCategories = () => {
+    const { categories } = this.state;
+    let arrCategories = [];
 
-    for (let i = 0; i < tags.length; i++) {
-      arrHTML.push(
-        <span key={i} className="result-company-tag">#{tags[i]}&nbsp;</span>
+    for(let i = 0; i< categories.length; i++) {
+      arrCategories.push(
+        <span key={i} className="result-company-category">
+          &nbsp;{categories[i]}&nbsp;
+        </span>
       );
     }
 
-    return arrHTML;
+    return arrCategories;
+  }
+
+  renderTags = () => {
+    const { tags } = this.state;
+    let arrTags = [];
+
+    for (let i = 0; i < tags.length; i++) {
+      arrTags.push(
+        <span key={i} className="result-company-tag">
+          <CheckIcon className="check-icon" style={{fontSize: 'large'}} />
+          {tags[i]}&nbsp;
+        </span>
+      );
+    }
+
+    return arrTags;
   }
 
   render() {
@@ -45,11 +63,12 @@ class Detail extends Component {
         <div className="company-imgs">
           <img src={imgs} alt="company-img" />
         </div>
+        <div className="header-card" style={{width: '100%'}}>
+          <div className="result-company-categories">{this.renderCategories()}</div>
+          <h5 className="company-name">{name}</h5>
+          <div className="share"><ShareOutlinedIcon /></div>
+        </div>
         <div className="company-info">
-          <div style={{width: '100%'}}>
-            <h5 className="company-name">{name}</h5>
-            <div className="share"><ShareOutlinedIcon /></div>
-          </div>
           <div className="result-company-tags">{this.renderTags()}</div>
           <p className="company-description">{desc}</p>
           {chartData ? (
